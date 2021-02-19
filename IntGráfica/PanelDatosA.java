e026eaf11050c60ce9adcd3c6e58f0eae1b02a43
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2021-02-19 10:25:58 -0600

    Update PanelDatosA.java
    Actualización de la clase Panel Datos A.

--- IntGráfica/PanelDatosA.java
+++ IntGráfica/PanelDatosA.java
@@ -1,38 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class PanelDatosA extends JPanel
+public class PanelDatosA extends JPanel implements ActionListener
 {
+  JButton btnGuardar, btnSalir;//visibilidad a nivel de clase
+  JTextField txtNombre, txtApellidos, txtTelefono;
+  JRadioButton amigo, conocido;
+  ManejoPersona datos = new ManejoPersona();
   
-  public PanelDatosA()
+  public PanelDatosB()
   {
     setLayout(new GridLayout(5,2));
     JLabel lblNombre = new JLabel("Nombre: ", JLabel.RIGHT);
-    JTextField txtNombre = new JTextField(20);
+    txtNombre = new JTextField(20);
     add(lblNombre);
     add(txtNombre);
+    
     JLabel lblApellidos = new JLabel("Apellidos: ", JLabel.RIGHT);
-    JTextField txtApellidos = new JTextField(30);
+    txtApellidos = new JTextField(30);
     add(lblApellidos);
     add(txtApellidos);
-    JLabel lblTelefono = new JLabel("Celular: ", JLabel.RIGHT);
-    JTextField txtTelefono = new JTextField(15);
+    
+    JLabel lblTelefono = new JLabel("Telefono: ", JLabel.RIGHT);
+    txtTelefono = new JTextField(15);
     add(lblTelefono);
     add(txtTelefono);
     
-    ButtonGroup gpoBotones = new ButtonGroup();
-    JRadioButton amigo = new JRadioButton("Amigo", true);
-    JRadioButton conocido = new JRadioButton("Conocido");
-    gpoBotones.add(amigo);
-    gpoBotones.add(conocido);
-    
+    ButtonGroup gpoRBotones = new ButtonGroup();
+    amigo = new JRadioButton("Amigo", true);
+    conocido = new JRadioButton("Conocido");
+    gpoRBotones.add(amigo);
+    gpoRBotones.add(conocido);
     add(amigo);
     add(conocido);
     
-    JButton btnGuardar = new JButton("Guardar");
-    JButton btnSalir = new JButton("Salir");
-    
+    btnGuardar = new JButton("Guardar");
+    btnGuardar.addActionListener(this);
+    btnSalir   = new JButton("Salir");
+    btnSalir.addActionListener(this);
     add(btnGuardar);
     add(btnSalir);
+  }//Método Constructor
+  
+  public void actionPerformed(ActionEvent e)
+  {
+    String tipo="Conocido";
+    
+    if (e.getSource() == btnGuardar) 
+      {
+         if(amigo.isSelected())
+                tipo="Amigo";
+         if(txtNombre.getText() == null || txtNombre.getText().isEmpty())
+         {
+          JOptionPane.showMessageDialog(null, "Debe de ir al menos el nombre de la persona",
+            "Aviso", JOptionPane.INFORMATION_MESSAGE);
+            txtNombre.requestFocus();
+         }
+        else
+         if(txtTelefono.getText() == null || txtTelefono.getText().isEmpty())
+         {
+          JOptionPane.showMessageDialog(null, "Se debe de colocar el teléfono de la persona",
+            "Aviso", JOptionPane.INFORMATION_MESSAGE);
+            txtTelefono.requestFocus();
+         }
+        else
+          {
+           datos.agregar(txtNombre.getText(), txtApellidos.getText(),
+           txtTelefono.getText(),tipo);
+           JOptionPane.showMessageDialog(null, "Se guardarán los datos",
+              "Aviso", JOptionPane.INFORMATION_MESSAGE);	
+            txtNombre.setText("");
+            txtApellidos.setText("");
+            txtTelefono.setText("");
+            txtNombre.requestFocus();
+           }
+         }
+		if (e.getSource() == btnSalir) {
+         datos.mostrar();
+			System.exit(0);
+        }
   }
 }
