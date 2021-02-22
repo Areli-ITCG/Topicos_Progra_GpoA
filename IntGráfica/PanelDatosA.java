ef30596f1d92968c3f40fd75f9aa5a8d7bcd5d1d
Areli Pérez <66649193+Areli-ITCG@users.noreply.github.com>
2021-02-22 11:05:16 -0600

    Update PanelDatosA.java
    Actualización de PanelDatosA.

--- IntGráfica/PanelDatosA.java
+++ IntGráfica/PanelDatosA.java
@@ -4,10 +4,10 @@ import java.awt.event.*;
 
 public class PanelDatosA extends JPanel implements ActionListener
 {
-  JButton btnGuardar, btnSalir;//visibilidad a nivel de clase
-  JTextField txtNombre, txtApellidos, txtTelefono;
-  JRadioButton amigo, conocido;
-  ManejoPersona datos = new ManejoPersona();
+  private JButton btnAceptar, btnCancelar;
+  private JTextField txtNombre, txtApellidos, txtTelefono;
+  private JRadioButton amigo, conocido;
+  ManejoPersona mpA = new ManejoPersona();
   
   public PanelDatosA()
   {
@@ -16,69 +16,67 @@ public class PanelDatosA extends JPanel implements ActionListener
     txtNombre = new JTextField(20);
     add(lblNombre);
     add(txtNombre);
-    
     JLabel lblApellidos = new JLabel("Apellidos: ", JLabel.RIGHT);
     txtApellidos = new JTextField(30);
     add(lblApellidos);
     add(txtApellidos);
-    
-    JLabel lblTelefono = new JLabel("Telefono: ", JLabel.RIGHT);
+    JLabel lblTelefono = new JLabel("Celular: ", JLabel.RIGHT);
     txtTelefono = new JTextField(15);
     add(lblTelefono);
     add(txtTelefono);
     
-    ButtonGroup gpoRBotones = new ButtonGroup();
+    ButtonGroup gpoBotones = new ButtonGroup();
     amigo = new JRadioButton("Amigo", true);
     conocido = new JRadioButton("Conocido");
-    gpoRBotones.add(amigo);
-    gpoRBotones.add(conocido);
+    gpoBotones.add(amigo);
+    gpoBotones.add(conocido);
+    
     add(amigo);
     add(conocido);
+     
+     btnAceptar = new JButton("Guardar");
+     btnAceptar.addActionListener(this);
+     btnCancelar = new JButton("Cancelar");
+     btnCancelar.addActionListener(this);
     
-    btnGuardar = new JButton("Guardar");
-    btnGuardar.addActionListener(this);
-    btnSalir   = new JButton("Salir");
-    btnSalir.addActionListener(this);
-    add(btnGuardar);
-    add(btnSalir);
-  }//Método Constructor
+    add(btnAceptar);
+    add(btnCancelar);
+  }
   
-  public void actionPerformed(ActionEvent e)
-  {
-    String tipo="Conocido";
-    
-    if (e.getSource() == btnGuardar) 
-      {
-         if(amigo.isSelected())
-                tipo="Amigo";
-         if(txtNombre.getText() == null || txtNombre.getText().isEmpty())
-         {
-          JOptionPane.showMessageDialog(null, "Debe de ir al menos el nombre de la persona",
+   public void actionPerformed(ActionEvent e) {
+     String tipo = "Amigo";
+     if(conocido.isSelected())
+        tipo="Conocido";
+        
+     if (e.getSource()==btnAceptar) {
+         if (txtNombre.getText()==null||txtNombre.getText().isEmpty())
+          { 
+            JOptionPane.showMessageDialog(null, "Debes colocar al menos el nombre",
             "Aviso", JOptionPane.INFORMATION_MESSAGE);
-            txtNombre.requestFocus();
-         }
-        else
-         if(txtTelefono.getText() == null || txtTelefono.getText().isEmpty())
-         {
-          JOptionPane.showMessageDialog(null, "Se debe de colocar el teléfono de la persona",
+           txtNombre.requestFocus();
+          }
+         else
+           if(txtTelefono.getText()==null||txtTelefono.getText().isEmpty()) 
+           {
+           JOptionPane.showMessageDialog(null, "Debes capturar su teléfono",
             "Aviso", JOptionPane.INFORMATION_MESSAGE);
             txtTelefono.requestFocus();
-         }
-        else
-          {
-           datos.agregar(txtNombre.getText(), txtApellidos.getText(),
-           txtTelefono.getText(),tipo);
-           JOptionPane.showMessageDialog(null, "Se guardarán los datos",
-              "Aviso", JOptionPane.INFORMATION_MESSAGE);	
-            txtNombre.setText("");
-            txtApellidos.setText("");
+            }  
+          else
+          {  
+            mpA.agregar(txtNombre.getText(), txtApellidos.getText(), txtTelefono.getText(), tipo);
+		  		JOptionPane.showMessageDialog(null, "Se guardarán los datos",
+            "Aviso", JOptionPane.INFORMATION_MESSAGE);
+            txtNombre.setText(null);		
             txtTelefono.setText("");
+            txtApellidos.setText("");
             txtNombre.requestFocus();
-           }
-         }
-		if (e.getSource() == btnSalir) {
-         datos.mostrar();
+          }
+      }
+		
+      if (e.getSource()==btnCancelar) {
+         mpA.mostrar();
 			System.exit(0);
-        }
-  }
+		}
+	}
 }
